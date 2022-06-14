c7c1ce68f3f26d7c0cd9bcb8d11db9078cf6c7bd
Gary, Lin <bugofbook@gmail.com>
2022-06-14 10:16:14 +0800

    add lib

--- apps/table-game/src/App.tsx
+++ apps/table-game/src/App.tsx
@@ -2,20 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import { Login } from "./page/login";
 import { Root } from "./layout/root";
 import { Main } from "./page/main";
-import { OthelloPage } from './page/catch/othello'
-import { TicTacToePage } from './page/connect/tictactoe'
-import { TicTacToeSpecialPage } from './page/connect/tictactoespecial'
-import { GomokuPage } from './page/connect/gomoku'
+import { OthelloPage } from './page/catchchess/othello'
+import { TicTacToePage } from './page/connectchess/tictactoe'
+import { TicTacToeSpecialPage } from './page/connectchess/tictactoespecial'
+import { GomokuPage } from './page/connectchess/gomoku'
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Root />}>
-        <Route path="/othello" element={<OthelloPage />} />
-        <Route path="/tictactoe" element={<TicTacToePage />} />
-        <Route path="/tictactoespecial" element={<TicTacToeSpecialPage />} />
-        <Route path="/gomoku" element={<GomokuPage />} />
+        <Route path="/catchchess/othello" element={<OthelloPage />} />
+        <Route path="/connectchess/tictactoe" element={<TicTacToePage />} />
+        <Route path="/connectchess/tictactoespecial" element={<TicTacToeSpecialPage />} />
+        <Route path="/connectchess/gomoku" element={<GomokuPage />} />
         <Route path="" element={<Main />} />
       </Route>
     </Routes>
---
+++ libs/reacthook/chess/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'reacthook-chess',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/reacthook/chess',
+};
