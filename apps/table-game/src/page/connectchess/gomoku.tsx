d9ff86a1008984b78e274b76c7e5d66856c55949
林裕敏 <bugofbook@gmail.com>
2023-06-08 13:41:49 +0800

    fix: :bug: gomoku connect over 5 bug

--- apps/table-game/src/page/connectchess/gomoku.tsx
+++ apps/table-game/src/page/connectchess/gomoku.tsx
@@ -5,8 +5,20 @@ import { GameInFo, GameMain } from "@my-app-game/react-ui/game";
 import {GomokuInitialData} from "@my-app-game/chess/board/oldInitialstatedata";
 import { SquaresDeepCopy } from "@my-app-game/chess/chess/oldGameBasic";
 import { addNewChess, ConeectJudge } from '@my-app-game/chess/connect/oldConnectGame';
+// import { addNewChess, ConeectJudge, willConnect } from '@my-app-game/chess/connect/oldConnectGame';
 import useCenterHook from "@my-app-game/reacthook/chess/oldpush";
 // markup
+
+
+const markEnforce = (currentBoard, gameinfo) => {
+  // if (gameinfo.turns === 0) {
+  //   return currentBoard
+  // }
+  // const chess = gameinfo.actionlists[gameinfo.turns - 1]
+  // const nextPlayerChesses = willConnect(chess, currentBoard.squares)
+  // console.log('nextPlayerChesses', nextPlayerChesses)
+  return currentBoard
+}
 export const GomokuPage = () => {
   const {history, gameinfo, jumpto, oneClick} = useCenterHook({initialstate: GomokuInitialData, canputJudge: canTicTacToePut, mainchange: tictactoeMainchange});
   const currentBoard = history[gameinfo.turns]
@@ -15,27 +27,28 @@ export const GomokuPage = () => {
     player2: "Jerry",
   }
   const gamerule = TicTacToeRule
+  const board = markEnforce(currentBoard, gameinfo)
   return (
     <>
       <GameMain
-        currentBoard={currentBoard}
+        currentBoard={board}
         gameinfo={gameinfo}
         players={players}
         gamerule={gamerule}
         mainchange={oneClick}
       />
-      <GameInFo 
-        players={players} 
-        history={history} 
-        gameinfo={gameinfo} 
-        // adddata={()=>this.adddata(info.gamename,info.actionlists)} 
+      <GameInFo
+        players={players}
+        history={history}
+        gameinfo={gameinfo}
+        // adddata={()=>this.adddata(info.gamename,info.actionlists)}
         jumpto={(step) => jumpto(step)}
       />
     </>
   )
 }
 
-const TicTacToeRule =  
+const TicTacToeRule =
 	<>
 		<li>雙方輪流放子</li>
 		<li>先連成一線者得勝</li>
